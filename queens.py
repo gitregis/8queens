ce8a53ea950c621bc48993ddcc6d4cd8a15f54c9
gitregis <director.eviaticum@gmail.com>
2019-01-07 15:12:20 -0600

    Update queens.py
    añado enlaces de descripcion de los modulos utilizados

--- queens.py
+++ queens.py
@@ -1,15 +1,3 @@
-#For this problem we assume each represent the coordinate of each queen on a vector.
-#The number on the element represents the row while the position on the vector represents the column.
-#The coordinates for the picture above is given as:
-
-#[3, 0, 4, 7, 5, 2, 6, 1]
-
-#We are eliminating Rook moves finding permutations of rows. This will ensure that the rows and columns do not overlap.
-#Bishop moves are eliminated by using set() and number of queens, N. We will check Bishop for both diagonals. If there is no overlap, the set of 8 diagonals will be unique and will have lenght 8.
-
-
-
-
 # itertools
 # https://docs.python.org/3.5/library/itertools.html
 
@@ -17,7 +5,18 @@
 # https://docs.python.org/3.5/library/itertools.html?highlight=permutations#itertools.permutations
 
 #lens ## https://docs.python.org/3/library/functions.html#len
-# len(s) Return the length (the number of items) of an object. The argument may be a sequence (such as a string, bytes, tuple, list, or range) or a collection (such as a dictionary, set, or frozen set).
+
+#len(s) Return the length (the number of items) of an object. The argument may be a sequence (such as a string, bytes, tuple, list, or range) or a collection (such as a dictionary, set, or frozen set).
+
+#For this problem we assume each represent the coordinate of each queen on a vector.
+#The number on the element represents the row while the position on the vector represents the column.
+#The coordinates for the picture above is given as:
+
+#[3, 0, 4, 7, 5, 2, 6, 1]#
+
+#We are eliminating Rook moves finding permutations of rows. This will ensure that the rows and columns do not overlap.
+#Bishop moves are eliminated by using set() and number of queens, N. 
+#We will check Bishop for both diagonals. If there is no overlap, the set of 8 diagonals will be unique and will have lenght 8.
 
 from itertools import permutations
 
